c19d2319bffe5294e293c3a0ea37adee741dbb11
kamscode <kamnoah88@gmail.com>
2023-04-02 13:44:46 +0300

    weather conversion program

---
+++ weather_conevrsion.js
@@ -0,0 +1,12 @@
+//The kelvin value stays constant throughout
+const kelvin = 500;
+//convert kelvin to celsius
+const celsius = kelvin - 273;
+//convert celsius to fahrenheit
+let fahrenheit = celsius * (9 / 5) + 32;
+//round down fahrenheit variable
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+let newton = celsius * (33 / 100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} in newton`);
