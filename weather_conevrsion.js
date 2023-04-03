651b9f8b593b7dd89859a46312deb3db657860c7
kamscode <kamnoah88@gmail.com>
2023-04-03 12:01:47 +0300

    Magic eight Ball added

--- weather_conevrsion.js
+++ weather_conevrsion.js
@@ -6,7 +6,7 @@ const celsius = kelvin - 273;
 let fahrenheit = celsius * (9 / 5) + 32;
 //round down fahrenheit variables
 fahrenheit = Math.floor(fahrenheit);
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+console.log(`The temperature is ${fahrenheit} degree's Fahrenheit`);
 let newton = celsius * (33 / 100);
 newton = Math.floor(newton);
 console.log(`The temperature is ${newton} in newton`);
